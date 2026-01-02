fe8b43bf6870cedf5f7ca70f92d212507e9d6a22
Elika <rey048286@gmail.com>
2026-01-02 22:27:32 +0800

    update modularity

--- include/sidebar.php
+++ include/sidebar.php
@@ -13,13 +13,13 @@
             </li>
             <li>
                 <a href="dashboard-list-courses.php" class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-[#708238] group">
-                    <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-[#708238]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v14M9 5v14M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/></svg>
+                    <i class="fa-solid fa-microphone-lines"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Courses</span>
                 </a>
             </li>
             <li>
                 <a href="dashboard-list-mentor.php" class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-[#708238] group">
-                    <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-[#708238]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 13h3.439a.991.991 0 0 1 .908.6 3.978 3.978 0 0 0 7.306 0 .99.99 0 0 1 .908-.6H20M4 13v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-6M4 13l2-9h12l2 9M9 7h6m-7 3h8"/></svg>
+                    <i class="fa-solid fa-chalkboard-user"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Mentor</span>
                 </a>
             </li>
